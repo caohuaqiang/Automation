8167c6e3fae4086fb537fb4a7646895e05a6e6d3
caohuaqiang <342473195@qq.com>
2018-04-26 13:10:00 +0800

    utf8-编码

--- pyinterface/runtests.py
+++ pyinterface/runtests.py
@@ -5,7 +5,7 @@ sys.path.append('./db_fixture')
 from HTMLTestRunner import HTMLTestRunner
 import unittest
 
-
+# 指定测试用例为当前目录下的interface目录
 test_dir = './interface'
 discover = unittest.defaultTestLoader.discover(test_dir, pattern='jf*.py')
 
