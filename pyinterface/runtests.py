8d3b949e5d06bd6034908f7479f166d3190c29b6
caohuaqiang <342473195@qq.com>
2018-04-26 12:25:07 +0800

    wocaocaocaococoac

--- pyinterface/runtests.py
+++ pyinterface/runtests.py
@@ -5,7 +5,7 @@ sys.path.append('./db_fixture')
 from HTMLTestRunner import HTMLTestRunner
 import unittest
 
-# 指定测试用例为当前目录下的interface目录
+
 test_dir = './interface'
 discover = unittest.defaultTestLoader.discover(test_dir, pattern='jf*.py')
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     filename = './report/' + now + '_result.html'
     fp = open(filename, 'wb')
     runner = HTMLTestRunner(stream=fp,
-                            title='曹华强接口测试报告',
-                            description='接口测试用例执行情况报告')
+                            title='caohuaqiangtestreport',
+                            description='report for test')
     runner.run(discover)
     fp.close()
