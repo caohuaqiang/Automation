10f4bcac883e979c150dac8d75d165703e868a1c
caohuaqiang <342473195@qq.com>
2018-04-26 13:27:00 +0800

    utf8-编码4.0

--- pyinterface/runtests.py
+++ pyinterface/runtests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*
 import time, sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.abspath(__file__))
 sys.path.append('./interface')
 sys.path.append('./db_fixture')
@@ -8,7 +9,7 @@ from HTMLTestRunner import HTMLTestRunner
 import unittest
 
 # 指定测试用例为当前目录下的interface目录
-test_dir = './interface'
+test_dir = 'Automation/pyinterface/interface'
 discover = unittest.defaultTestLoader.discover(test_dir, pattern='jf*.py')
 
 
