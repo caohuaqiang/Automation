a6c37f184f1cb617389e650de960ffacaa4e8d3e
caohuaqiang <342473195@qq.com>
2018-04-29 23:03:05 +0800

    自动发标脚本独立化2.0

--- auto/case/Management.py
+++ auto/case/Management.py
@@ -2,6 +2,7 @@
 #!/usr/bin/env python
 import sys, os
 sys.path.append(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))     # auto文件夹路径
 sys.path.insert(0, parentdir)
 from pprint import pprint
